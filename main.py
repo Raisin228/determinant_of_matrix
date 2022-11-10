cc7702daf59abfdcb0f44953dde5abb3586aed5c
Raisin228 <111242834+Raisin228@users.noreply.github.com>
2022-11-10 21:05:06 +0300

    Add files via upload
    fix bug with output recursion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # <<<<<<<<<-----------------the beginning of the code associated with the recursive Laplace formula----------->>>>>>>>>>>>>
+# we create a global list that will store the results of the minors
+List_res = []
 # this procedure outputs matrices in a beautiful way
 def output_matrix(mat):
     for i in mat:
@@ -35,13 +37,16 @@ def zero_column(mat):
     return ind_column, max_counter
 
 # this function is needed in order to beautifully output 1 recursive decomposition action
-def beautiful_output(k, r, c, r_z, c_z, mat, new_mat, elem, List_res):
+def beautiful_output(k, r, c, mat, new_mat, elem):
+    global List_res
+    global r_z, c_z
+    global const_len
     # if there has not been more than one decomposition, then you need to output a message
     # which says by rows or columns there will be decomposition
-    if k == 0 and r[0] == r_z[0] and c[0] == c_z[0]:
+    if k == 0 and r[0] == r_z[0] and c[0] == c_z[0] and const_len == len(mat):
         print(f'Decomposition of the determinant by {r[0] + 1} lines' if r[1] > c[1] or r[1] == c[1] else f'Decomposition of the determinant into {c[0] + 1} columns')
     # if element is not equal to 0 then we output this element and an additional minor to it
-    if r[0] == r_z[0] and c[0] == c_z[0] and elem[0] != 0:
+    if r[0] == r_z[0] and c[0] == c_z[0] and elem[0] != 0 and len(new_mat) == const_len - 1:
         print(elem[0])
         output_matrix(new_mat)
         # I think we need to put the result of the recursion in a variable so as not to count the same thing many times
@@ -55,7 +60,7 @@ def beautiful_output(k, r, c, r_z, c_z, mat, new_mat, elem, List_res):
             print(f'= {elem[0]} * {next_step} = {elem[0] * next_step}')
             List_res.append(elem[0] * next_step)
     # when we have reached the last step, we output a message with the result of recursion
-    if r[0] == r_z[0] and c[0] == c_z[0] and k == len(mat) - 1:
+    if r[0] == r_z[0] and c[0] == c_z[0] and k == len(mat) - 1 and len(new_mat) == const_len - 1:
         print('The determinant of the matrix by the recursive formula:', end=' ')
         for i in range(len(List_res)):
             # put brackets if we need to it
@@ -67,6 +72,7 @@ def beautiful_output(k, r, c, r_z, c_z, mat, new_mat, elem, List_res):
 
 # function with BY THE RECURSIVE LAPLACE FORMULA
 def rec_laplace(mat):
+    global r_z, c_z
     # condition for exiting recursion
     if (len(mat) == 1):
         return mat[0][0]
@@ -81,11 +87,7 @@ def rec_laplace(mat):
                 new_mat = [q for q in new_mat if len(q) != 0]
                 # chose sign element
                 elem = [mat[r[0]][k] if (r[0] + k) % 2 == 0 else -mat[r[0]][k]]
-                # if we go into recursion 1 time then we create a global list that will store the results of the minors
-                if k == 0 and r[0] == r_z[0] and c[0] == c_z[0]:
-                    global List_res
-                    List_res = []
-                beautiful_output(k, r, c, r_z, c_z, mat, new_mat, elem, List_res)
+                beautiful_output(k, r, c, mat, new_mat, elem)
 
                 # if el = 0 then we can don't call recursion
                 if mat[r[0]][k] != 0:
@@ -99,9 +101,7 @@ def rec_laplace(mat):
                 new_mat = [[mat[i][j] for j in range(len(mat)) if i != k and j != c[0]] for i in range(0, len(mat))]
                 new_mat = [q for q in new_mat if len(q) != 0]
                 elem = [mat[k][c[0]] if (c[0] + k) % 2 == 0 else -mat[k][c[0]]]
-                if k == 0 and r[0] == r_z[0] and c[0] == c_z[0]:
-                    List_res = []
-                beautiful_output(k, r, c, r_z, c_z, mat, new_mat, elem, List_res)
+                beautiful_output(k, r, c, mat, new_mat, elem)
 
                 if mat[k][c[0]] != 0:
                     ans += elem[0] * rec_laplace(new_mat)
@@ -116,8 +116,8 @@ print()
 with open(name_file) as file:
     size_m = int(file.readline()) # reading first string
     matrix = [[int (j) for j in file.readline().split()] for i in range(size_m)] # reading matrix from file
-    global r_z, c_z # need to definitions of the first step of entering recursion
-    r_z, c_z = zero_row(matrix), zero_column(matrix)
+    const_len = len(matrix)
+    r_z, c_z = zero_row(matrix), zero_column(matrix) # need to definitions of the first step of entering recursion
     print("<<Your matrix from file>>")
     output_matrix(matrix)
     # if the matrix consists of only one element the result will be this element
